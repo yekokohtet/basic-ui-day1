fa84d5f74469d166f669ccac4ec662052b7f6511
Ye Ko Ko Htet <yekokohtet@mm-digital-solutions.com>
2019-07-27 14:36:11 +0630

    -show label

---
+++ Day-1/BasicUI-Day1/BasicUI-Day1/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  BasicUI-Day1
+//
+//  Created by Ye Ko Ko Htet on 27/07/2019.
+//  Copyright © 2019 Ye Ko Ko Htet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var lblShowName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tfName.delegate = self
+    }
+
+    @IBAction func btnShowName(_ sender: Any) {
+        lblShowName.text = tfName.text
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        tfName.resignFirstResponder()
+        return true
+    }
+    
+}
+
